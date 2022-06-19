d1efdc817e706ceebc7ca805ef771cf8e2dad8e6
Collin Ahn <collinahn@hufs.ac.kr>
2022-06-19 23:22:27 +0900

    add file util classes
    1. 파일을 크기별로 비교한 후
    2. 같은 크기의 파일이 있다면 파일의 일부를 불러들여 해시한다.
    3. 해시값을 비교해 같은 파일인지 따진다.
    4. 삭제/이동(추가예정)하는 클래스도 추가

---
+++ file_utils/compare.py
@@ -0,0 +1,60 @@
+from utils.properties import FileProperty
+
+
+class CompareFile:
+    '''
+    1차적으로 크기 비교, 크기가 같다면 해시값을 비교한다.
+    '''
+    def __init__(self, file_list) -> None:
+        self.file_list: list[str] = file_list
+        self.file_dict: dict[str,FileProperty] = {
+            file_path:FileProperty(file_path)
+            for file_path in self.file_list
+        }
+
+        cmp_size = self._compare_size()
+        cmp_hash = self._compare_hash(cmp_size)
+        self.result: list[tuple[str]] = cmp_hash
+
+    def _compare_size(self):
+        cmp_size: dict[int,list] = {} 
+        for file_path, file_prop in self.file_dict.items():
+            if file_prop.size in cmp_size:
+                cmp_size.get(file_prop.size).append(file_path)
+                continue
+            
+            cmp_size[file_prop.size] = [file_path, ]
+
+        return cmp_size
+            
+    def _compare_hash(self, size_res: dict):
+        identical_file_list: list[tuple[str]] = []
+        for size, file_list in size_res.items():
+            if len(file_list) == 1: continue
+            self._load_hash_value(file_list)
+
+            # 해시값이 같은 파일들 분류
+            hash_dict: dict[int,list] = {}
+            for file_dir in file_list:
+                hash_value = self.file_dict[file_dir].hash
+                if hash_value in hash_dict:
+                    hash_dict[hash_value].append(file_dir)
+                    continue
+
+                hash_dict[hash_value] = [file_dir, ]
+
+            # 분류된 것 확인 후 하나 이상이면 리스트 추가
+            identical_file_list.extend(
+                tuple(sorted_file_list) 
+                for sorted_file_list in hash_dict.values() 
+                if len(sorted_file_list) > 1
+            )
+                        
+        return identical_file_list
+
+    def _load_hash_value(self, file_list):
+        for file_dir in file_list:
+            try:
+                self.file_dict[file_dir].init_hash()
+            except AttributeError:
+                continue
