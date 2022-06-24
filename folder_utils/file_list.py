3b4cb7b99e2c1f3c53485fd8c06ab14fad12002c
Collin Ahn <collinahn@hufs.ac.kr>
2022-06-25 00:05:24 +0900

    update 메소드 변경
    static method로 변경

--- folder_utils/file_list.py
+++ folder_utils/file_list.py
@@ -6,20 +6,26 @@ from utils.constants import TEMP_FILE_PREFIX
 
 class FileList:
     def __init__(self, path: str) -> None:
-        self.walk_path = tuple(os.walk(path))
-        self.walk_path_filtered = self.filter_forbidden()
-        self.contents = self.only_media()
+        self.path = path
+        self.contents = []
+
+    def lazy_init(self):
+        self.walk_path = tuple(os.walk(self.path))
+        self.walk_path_filtered = self.filter_forbidden(self.walk_path)
+        self.contents = self.only_media(self.walk_path_filtered)
         self.count = len(self.contents)
 
-    def filter_forbidden(self) -> tuple:
+    @staticmethod
+    def filter_forbidden(walk_path) -> tuple:
         return tuple( 
-            pack for pack in self.walk_path 
+            pack for pack in walk_path 
             if not os.path.basename(pack[0]).startswith(('.', '_')) 
         )
 
-    def only_media(self):
+    @staticmethod
+    def only_media(walk_path_filtered):
         res = []
-        for pack in self.walk_path_filtered:
+        for pack in walk_path_filtered:
             res.extend(
                 os.path.join(pack[0], file_name) 
                 for file_name in pack[2] 
