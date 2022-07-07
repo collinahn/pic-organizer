3a294eb94dd9576df963c243285e171977102cdf
Collin Ahn <collinahn@hufs.ac.kr>
2022-07-07 14:34:08 +0900

    add QPushButton Style

--- qt/stylesheets.py
+++ qt/stylesheets.py
@@ -2,6 +2,15 @@ main_wigdet_stylesheet = '''
 QGroupBox { 
     border: 0px; 
 }
+
+QWidget .QPushButton {
+    border: 0.5px solid grey;
+    border-radius: 5px;
+    padding: 8px 16px 8px 16px;
+}
+QWidget .QPushButton:hover {
+    border:2px solid grey;
+}
 '''
 
 flat_push_button = '''
